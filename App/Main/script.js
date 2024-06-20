d0f309a90bb7bad4e6df85c5d73b126865086b6c
Kall00 <100430886+Kall00@users.noreply.github.com>
2024-06-20 10:27:17 +0000

    Valid moves of bishop and queen added

--- App/Main/script.js
+++ App/Main/script.js
@@ -70,11 +70,11 @@ valid_moves=getPawnmoves(clicked_src,id);
   }
   
   else if (clicked_src.includes("bishop")){
-    valid_moves=getBishopmoves(clicked_src,id);
+    valid_moves=getBishopmoves(id);
   }
   
   else if (clicked_src.includes("queen")){
-    valid_moves=getQueenmoves(clicked_src,id);
+    valid_moves=getQueenmoves(id);
   }
   
   else if (clicked_src.includes("king")){
@@ -234,87 +234,9 @@ temp_index=x_mark.indexOf(id.charAt(0));
 // Contains repetitive code 
 // not able to correct that
 function getRookmoves(id){
-
-  let valid_moves=[];
-
-  // getting index
-let x_index=x_mark.indexOf(id.charAt(0));
-let y_index=id.charAt(1);
   
-  // move along y
-
-  // for (suppose chess pcs at y index 5 then only for 6,7,8 places)
-  for (let i = (parseInt(id.charAt(1))+1); i <= 8; i++){
-    if(!has_chess_pieces(x_mark[x_index]+i)){
-      valid_moves.push({e:get_element_by_id(x_mark[x_index]+i),clr:"green"});
-      
-    }else {
-
-      // checking for opposite color chess pcs
-      if (!get_element_by_id(x_mark[x_index]+i).getElementsByTagName('img').item(0).alt.startsWith(turn_of)){
-        valid_moves.push({e:get_element_by_id(x_mark[x_index]+i),clr:"red"});
-      }
-      
-      break;
-  }
-    
-  }
-
-  // for (suppose chesspcs at y index 5 then only for 1,2,3,4 places)
-
-  for (let i = (parseInt(id.charAt(1))-1); i>=1; i--){
-    if(!has_chess_pieces(x_mark[x_index]+i)){
-      valid_moves.push({e:get_element_by_id(x_mark[x_index]+i),clr:"green"});
-      
-    }else {
-
-      // checking for opposite color chess pcs
-      if (!get_element_by_id(x_mark[x_index]+i).getElementsByTagName('img').item(0).alt.startsWith(turn_of)){
-        valid_moves.push({e:get_element_by_id(x_mark[x_index]+i),clr:"red"});
-      }
-      
-      break;
-    }
-
-  }
-
-  // move along x index
-  // for (suppose chess pcs at y index E then only for F,G,H places)
-  for (let i = (x_index+1); i <= 7; i++){
-    if(!has_chess_pieces(x_mark[i]+y_index)){
-      valid_moves.push({e:get_element_by_id(x_mark[i]+y_index),clr:"green"});
-      
-    }else {
-
-      // checking for opposite color chess pcs
-      if (!get_element_by_id(x_mark[i]+y_index).getElementsByTagName('img').item(0).alt.startsWith(turn_of)){
-        valid_moves.push({e:get_element_by_id(x_mark[i]+y_index),clr:"red"});
-      }
-      
-      break;
-  }
-    
-  }
-
-  // for (suppose chess pcs at y index E then only for A,B,C,D places)
-  for (let i = (x_index-1); i >= 0; i--){
-    if(!has_chess_pieces(x_mark[i]+y_index)){
-      valid_moves.push({e:get_element_by_id(x_mark[i]+y_index),clr:"green"});
-      
-    }else {
-
-      // checking for opposite color chess pcs
-      if (!get_element_by_id(x_mark[i]+y_index).getElementsByTagName('img').item(0).alt.startsWith(turn_of)){
-        valid_moves.push({e:get_element_by_id(x_mark[i]+y_index),clr:"red"});
-      }
-      
-      break;
-  }
-    
-  }
+  return straight_moves(id);
   
-  
-  return valid_moves;
 }
 
 function getKnightmoves(id){
@@ -356,15 +278,18 @@ function getKnightmoves(id){
   return valid_moves;
 }
 
-function getBishopmoves(){
-  let valid_moves=[];
-
-  return valid_moves;
+function getBishopmoves(id){
+  
+  return diagonal_moves(id);
+  
 }
 
-function getQueenmoves(){
+function getQueenmoves(id){
   let valid_moves=[];
 
+  valid_moves.push(...straight_moves(id));
+valid_moves.push(...diagonal_moves(id));
+  
   return valid_moves;
 }
 
@@ -403,6 +328,197 @@ element.getElementsByTagName('img').item(0).remove()
 
   // change the turn 
   mng_turn();
+  
+}
+
+function straight_moves(id){
+
+  let valid_moves=[];
+  let temp_id=[];
+  let id_ri=[]; // right
+  let id_le=[]; // left
+  let id_up=[]; // upper
+  let id_lo=[]; // lower
+  
+
+  // getting index
+let x_index=x_mark.indexOf(id.charAt(0));
+let y_index=id.charAt(1);
+  
+  // move along y
+
+  // for (suppose chess pcs at y index 5 then only for 6,7,8 places) i.e upper
+  for (let i = (parseInt(id.charAt(1))+1); i <= 8; i++){
+    id_up.push(x_mark[x_index]+i);
+    
+  }
+
+  // for (suppose chesspcs at y index 5 then only for 1,2,3,4 places) i.e. lower
+
+  for (let i = (parseInt(id.charAt(1))-1); i>=1; i--){
+
+    id_lo.push(x_mark[x_index]+i);
+
+  }
+
+  // move along x index
+  
+  // for (suppose chess pcs at y index E then only for F,G,H places) i.e. right
+  for (let i = (x_index+1); i <= 7; i++){
+    id_ri.push(x_mark[i]+y_index);
+    
+  }
+
+  // for (suppose chess pcs at y index E then only for A,B,C,D places) i.e. left
+  for (let i = (x_index-1); i >= 0; i--){
+    id_le.push(x_mark[i]+y_index);
+    
+  }
+
+  // pushing the ids list to tempid
+  temp_id.push(id_ri);
+  temp_id.push(id_up);
+  temp_id.push(id_lo);
+  temp_id.push(id_le);
+
+  
+  // transferring ids to valid moves after some checks
+  temp_id.forEach((i) =>{
+
+     for(let j=0;j<=(i.length);j++){
+
+if (get_element_by_id(i[j])!=null){   
+  if(!has_chess_pieces(i[j])){
+      valid_moves.push({e:get_element_by_id(i[j]),clr:"green"});
+      
+    }else {
+
+      // checking for opposite color chess pcs
+      if (!get_element_by_id(i[j]).getElementsByTagName('img').item(0).alt.startsWith(turn_of)){
+        valid_moves.push({e:get_element_by_id(i[j]),clr:"red"});
+      }
+      
+      break;
+      }
+      
+}
+    }
+    
+  })
+  
+  
+  return valid_moves;
+  
+  
+}
+
+function diagonal_moves(id){
+
+ let valid_moves=[];
+
+  // getting index
+  let x_index=x_mark.indexOf(id.charAt(0));
+  let y_index=parseInt(id.charAt(1));
+
+  let temp_id=[];
+  let id_ru=[]; // right upper
+  let id_lu=[]; // left upper
+  let id_ll=[]; // left lower
+  let id_rl=[]; // right lower
+
+  // moves of left upper diagonal
+  for (let i = (y_index+1); i <= 8; i++){
+
+    id_lu.push(x_mark[x_index-1]+(y_index+1));
+
+    // updating the x index and y index at each step
+    x_index--;
+    y_index++;
+    
+  }
+
+  // reset the x index and y index
+  x_index=x_mark.indexOf(id.charAt(0));
+y_index=parseInt(id.charAt(1));
+  
+
+  // moves of right upper diagonal
+
+  for (let i = (y_index+1); i <= 8; i++){
+
+    id_ru.push(x_mark[x_index+1]+(y_index+1));
+
+    // updating the x index and y index at each step
+    x_index++;
+    y_index++;
+
+  }
+
+  // reset the x index and y index
+  x_index=x_mark.indexOf(id.charAt(0));
+y_index=parseInt(id.charAt(1));
+
+  // moves of left lower diagonal
+
+  for (let i = (y_index-1); i >=1; i--){
+
+    id_ll.push(x_mark[x_index-1]+(y_index-1));
+
+    // updating the x index and y index at each step
+    x_index--;
+    y_index--;
+
+  }
+
+  // reset the x index and y index
+    x_index=x_mark.indexOf(id.charAt(0));
+  y_index=parseInt(id.charAt(1));
+  
+  // moves of right lower diagonal
+
+  for (let i = (y_index-1); i >=1; i--){
+
+    id_rl.push(x_mark[x_index+1]+(y_index-1));
+
+    // updating the x index and y index at each step
+    x_index++;
+    y_index--;
+    
+  }
+
+  // pushing the ids list to tempid
+  temp_id.push(id_lu);
+  temp_id.push(id_ru);
+  temp_id.push(id_ll);
+  temp_id.push(id_rl);
+
+
+  // transferring ids to valid moves after some checks
+  temp_id.forEach((i) =>{
+
+     for(let j=0;j<=(i.length);j++){
+
+if (get_element_by_id(i[j])!=null){   
+  if(!has_chess_pieces(i[j])){
+      valid_moves.push({e:get_element_by_id(i[j]),clr:"green"});
+      
+    }else {
+
+      // checking for opposite color chess pcs
+      if (!get_element_by_id(i[j]).getElementsByTagName('img').item(0).alt.startsWith(turn_of)){
+        valid_moves.push({e:get_element_by_id(i[j]),clr:"red"});
+      }
+      
+      break;
+      }
+      
+}
+    }
+    
+  })
+
+  return valid_moves;
+  
   
 }
 
